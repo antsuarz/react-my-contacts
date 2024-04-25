281b90a8f4c25ad697e153b888ced632b4cb68e9
antsuarz <uo270543@uniovi.es>
2024-04-25 20:09:31 +0200

    Componentes Card, CardList y Scroll

--- react-my-contacts/src/App.js
+++ react-my-contacts/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import CardList from './components/CardList';
+import Scroll from './components/Scroll';
+
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=20')
+      .then(response => response.json())
+      .then(contacts => setContacts(contacts.results));
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='tc '>
+      <header>
+        <h1 className='f1'>My contacts</h1>
       </header>
+      {contacts.length === 0 ? <h2 className='f2'>Loading...</h2> :
+        <Scroll>
+          <CardList contacts={contacts} />
+        </Scroll>
+      }
+      <footer>
+        <hr /><p>Desarrollo de Software para Dispositivos Moviles.
+          {new Date().getFullYear()}</p>
+      </footer>
     </div>
   );
 }
+
 
 export default App;
