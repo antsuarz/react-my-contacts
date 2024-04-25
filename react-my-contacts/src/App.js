9ed726e05847b95a3c55e76b9812987b28ea0785
antsuarz <uo270543@uniovi.es>
2024-04-25 20:30:37 +0200

    Barra de Busqueda y componente de Errores

--- react-my-contacts/src/App.js
+++ react-my-contacts/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import CardList from './components/CardList';
 import Scroll from './components/Scroll';
+import Searcher from './components/Searcher';
 
 
 function App() {
@@ -11,15 +12,46 @@ function App() {
       .then(response => response.json())
       .then(contacts => setContacts(contacts.results));
   }, [])
+  const [searchField, setSearchField] = useState('');
+
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
+  }
+
+  const searchedContacts = contacts.filter(contact => {
+    return (contact.name['first'] + ' ' +
+      contact.name['last']).toLowerCase().includes(searchField.toLowerCase())
+  });
+
+  const onAZ = () => {
+    let az = contacts.sort((a, b) => {
+      return (a.name['first'] + " " +
+        a.name['last']).localeCompare(b.name['first'] + " " + b.name['last'])
+    })
+    setContacts([...az]); //clone the list
+  }
+  const onZA = () => {
+    let za = contacts.sort((a, b) => {
+      return (b.name['first'] + " " +
+        b.name['last']).localeCompare(a.name['first'] + " " + a.name['last'])
+    })
+    setContacts([...za]); //clone the list
+  }
+
+
   return (
     <div className='tc '>
       <header>
         <h1 className='f1'>My contacts</h1>
       </header>
       {contacts.length === 0 ? <h2 className='f2'>Loading...</h2> :
-        <Scroll>
-          <CardList contacts={contacts} />
-        </Scroll>
+        <>
+          <Searcher searchChange={onSearchChange}  az={onAZ} za={onZA} />
+          <Scroll>
+            <CardList contacts={onSearchChange} />
+          </Scroll>
+        </>
+
       }
       <footer>
         <hr /><p>Desarrollo de Software para Dispositivos Moviles.
